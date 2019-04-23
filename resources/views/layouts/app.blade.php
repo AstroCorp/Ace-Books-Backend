df6e71e00b1b0c7bb64a0e5a2014d004002fcd3c
Alejandro Leal <aleaberm12@gmail.com>
2019-04-23 14:39:48 +0200

    Problema en el sidebar solucionado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,6 +93,8 @@
                     </button>
                 </nav>
 
+                <div class="d-sm-none m-4 p-2"></div>
+
                 @if ($logo)
                     <div class="row m-0 my-3">
                         <div class="col-12 p-0 text-center text-md-right">

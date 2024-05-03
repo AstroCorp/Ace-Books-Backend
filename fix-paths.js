f10bded033596cafe0aadba2ac1e464cb9442b11
Alejandro Leal <aleaberm12@gmail.com>
2024-05-04 01:39:28 +0200

    fix paths

--- fix-paths.js
+++ fix-paths.js
@@ -2,21 +2,25 @@ const fs = require('fs');
 const path = require('path');
 
 // Define el directorio raíz donde se encuentran tus archivos TypeScript
-const rootDir = '/src'; // Cambia esto a tu ruta
+const rootDir = './src'; // Cambia esto a tu ruta
 
 // Recibe un directorio y retorna todos los archivos .ts encontrados
 const getAllTSFiles = (dir) => {
     let results = [];
     const list = fs.readdirSync(dir);
+
     list.forEach((file) => {
         file = path.join(dir, file);
+
         const stat = fs.statSync(file);
+
         if (stat && stat.isDirectory()) {
             results = results.concat(getAllTSFiles(file)); // Recursión para subdirectorios
         } else if (file.endsWith('.ts')) {
             results.push(file); // Añade archivos .ts a la lista
         }
     });
+
     return results;
 };
 
@@ -26,7 +30,8 @@ const replaceAtSymbol = (filePath, rootDir) => {
     const importRegex = /import\s+.*\s+from\s+['"]@\/(.*)['"]/g; // Encuentra importaciones con '@/'
 
     content = content.replace(importRegex, (match, p1) => {
-        const relativePath = path.relative(path.dirname(filePath), path.join(rootDir, p1));
+        const relativePath = path.relative(path.dirname(filePath), path.join(rootDir, p1)).replace(/\\/g, '/');
+
         return match.replace(`@/${p1}`, `./${relativePath}`);
     });
 
